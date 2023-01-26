737ca9412e3898605242b91b52c929a0ca603ec9
Tri Le <tri.le@tyme.com>
2023-01-26 12:55:39 +0700

    refactor: HttpServerHandler use AsciiString

--- src/main/kotlin/HttpServerHandler.kt
+++ src/main/kotlin/HttpServerHandler.kt
@@ -5,6 +5,7 @@ import io.netty.handler.codec.http.DefaultFullHttpResponse
 import io.netty.handler.codec.http.HttpRequest
 import io.netty.handler.codec.http.HttpResponseStatus
 import io.netty.handler.codec.http.HttpVersion
+import io.netty.util.AsciiString
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -12,15 +13,22 @@ import uti.Log
 
 class HttpServerHandler : SimpleChannelInboundHandler<HttpRequest>(), Log {
 
-  override fun channelRead0(ctx: ChannelHandlerContext, request: HttpRequest) {
+  private val responseContent = "Hello from HttpServerHandler"
+  private val CONTENT_TYPE = AsciiString.cached("Content-Type")
+  private val CONTENT_TYPE_TEXT_PLAIN = AsciiString.cached("text/plain")
+  private val CONTENT_LENGTH = AsciiString.cached("Content-Length")
+
+  override fun channelRead0(ctx: ChannelHandlerContext, msg: HttpRequest) {
     CoroutineScope(Dispatchers.IO).launch {
       val response = DefaultFullHttpResponse(
         HttpVersion.HTTP_1_1,
         HttpResponseStatus.OK,
-        Unpooled.wrappedBuffer("Hello".toByteArray())
+        Unpooled.wrappedBuffer(responseContent.toByteArray())
       )
-      response.headers().set("Content-Type", "text/plain")
-      response.headers().set("Content-Length", response.content().readableBytes())
+
+      response.headers().set(CONTENT_TYPE, CONTENT_TYPE_TEXT_PLAIN)
+      response.headers().setInt(CONTENT_LENGTH, response.content().readableBytes())
+
       ctx.writeAndFlush(response)
       ctx.close()
     }
