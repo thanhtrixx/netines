c8b067224f6252973daa705c0790ad7638b2b28d
Tri Le <tri.le@tyme.com>
2023-01-26 12:40:31 +0700

    fix: HttpServerHandler use FullHttpRequest -> HttpRequest

--- src/main/kotlin/HttpServerHandler.kt
+++ src/main/kotlin/HttpServerHandler.kt
@@ -2,7 +2,7 @@ import io.netty.buffer.Unpooled
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.SimpleChannelInboundHandler
 import io.netty.handler.codec.http.DefaultFullHttpResponse
-import io.netty.handler.codec.http.FullHttpRequest
+import io.netty.handler.codec.http.HttpRequest
 import io.netty.handler.codec.http.HttpResponseStatus
 import io.netty.handler.codec.http.HttpVersion
 import kotlinx.coroutines.CoroutineScope
@@ -10,9 +10,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import uti.Log
 
-class HttpServerHandler : SimpleChannelInboundHandler<FullHttpRequest>(), Log {
+class HttpServerHandler : SimpleChannelInboundHandler<HttpRequest>(), Log {
 
-  override fun channelRead0(ctx: ChannelHandlerContext, request: FullHttpRequest) {
+  override fun channelRead0(ctx: ChannelHandlerContext, request: HttpRequest) {
     CoroutineScope(Dispatchers.IO).launch {
       val response = DefaultFullHttpResponse(
         HttpVersion.HTTP_1_1,
@@ -23,7 +23,6 @@ class HttpServerHandler : SimpleChannelInboundHandler<FullHttpRequest>(), Log {
       response.headers().set("Content-Length", response.content().readableBytes())
       ctx.writeAndFlush(response)
       ctx.close()
-      l.info("Response sent")
     }
   }
 }
